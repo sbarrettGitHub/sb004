4ad7d76f7dd69092b4a9568602c7c03988dd04fd
sbarrettGitHub <barrettseamus@hotmail.com>
2014-12-01 23:27:03 +0000

    Improved image selection
    Can drag n drop, select file or paste link

--- app.js
+++ app.js
@@ -16,4 +16,18 @@ app.config(['$routeProvider',
       });
   }]);
 
+app.directive("ngFileSelect",function(){
 
+  return {
+    link: function($scope,el){
+      
+      el.bind("change", function(e){
+      
+        $scope.file = (e.srcElement || e.target).files[0];
+        $scope.getFile();
+      })
+      
+    }
+    
+  }
+})
--- newQuoteCtrl.js
+++ newQuoteCtrl.js
@@ -1,18 +1,34 @@
 'use strict';
 (function() {
 
-  var newQuoteCtrl = function($scope, $timeout) {
+  var newQuoteCtrl = function($scope, $timeout, fileReader) {
 	$scope.image = null;
 	$scope.imageFileName = null;
+	$scope.file = null;
+	$scope.loading = false;
 	$scope.$on("Drop.Url",function(event, url){
 		$timeout(function(){
 			$scope.image = url;
 			$scope.imageFileName = url;
 		}, 100);		
-	}); 	
+	}); 
+	$scope.getFile = function () {
+		$scope.loading = true;
+		$timeout(function(){
+			fileReader.readAsDataUrl($scope.file, $scope)
+			.then(function(result) {
+			  $scope.image = result;
+			  $scope.loading = false;
+		  });
+		}, 100);	
+        
+    };	
+	$scope.$on("fileProgress", function(e, progress) {
+        $scope.loading = true;
+    });
   }
 
   // Register the controller
-  app.controller('newQuoteCtrl', ["$scope", "$timeout", newQuoteCtrl]);
+  app.controller('newQuoteCtrl', ["$scope", "$timeout", "fileReader", newQuoteCtrl]);
 
 })();
