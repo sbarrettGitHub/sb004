593383c8b34dfdf923e93024a519f8ffb15d2c71
sbarrettGitHub <barrettseamus@hotmail.com>
2015-04-01 17:01:55 +0100

    Added beginnings of memeView

---
+++ SB004_Web/Scripts/app/controllers/memeViewCtrl.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+(function () {
+
+    var memeViewCtrl = function ($scope) {
+
+        $scope.waiting = false;
+        $scope.waitHeading = "Please wait...";
+        $scope.waitingMessage = "";
+
+
+        function startWaiting(heading, message) {
+            $scope.waiting = true;
+            $scope.waitHeading = !heading ? "Please wait..." : heading;
+            $scope.waitingMessage = !message ? "" : message;
+        }
+        function endWaiting() {
+            $scope.waiting = false;
+            $scope.waitHeading = "";
+            $scope.waitingMessage = "";
+        }
+    }
+
+    // Register the controller
+    app.controller('memeViewCtrl', ["$scope", memeViewCtrl]);
+
+})();
